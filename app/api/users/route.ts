fd99020b189e5a771b755b4bc5a74609230417c1
kenglory <samusenele@gmail.com>
2025-06-15 01:38:05 +0200

    Fix accesso JSON su Vercel

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,6 +1,10 @@
 import { NextResponse } from 'next/server';
-import users from '../../../data/users.json';
+import path from 'path';
+import { promises as fs } from 'fs';
 
 export async function GET() {
+  const filePath = path.join(process.cwd(), 'public/data/users.json');
+  const file = await fs.readFile(filePath, 'utf8');
+  const users = JSON.parse(file);
   return NextResponse.json(users);
 }
