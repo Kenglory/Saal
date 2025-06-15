d5868f3d5f789011098b55a90489c666f330c2df
kenglory <samusenele@gmail.com>
2025-06-15 23:19:11 +0200

    Fix Matteo JSX

---
+++ app/dashboard/matteo/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import SchedaMatteo from '@/components/SchedaMatteo';
+
+export default function MatteoPage() {
+  return (
+    <div className="p-4">
+      <SchedaMatteo />
+    </div>
+  );
+}
