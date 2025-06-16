9c5ff06e6dd46e2a35ba6e60b70f5ef1caeaf680
kenglory <samusenele@gmail.com>
2025-06-16 23:37:14 +0200

    prova schede base tutti parte 8

---
+++ app/dashboard/master/master.tsx
@@ -0,0 +1,11 @@
+// Master - master
+'use client';
+
+export function MasterPage() {
+  return (
+    <div className="p-6 text-center">
+      <h1 className="text-3xl font-bold">👑 Master</h1>
+      <p className="mt-4 text-yellow-500 italic">Pagina di controllo del master (in arrivo) 🧠</p>
+    </div>
+  );
+}
