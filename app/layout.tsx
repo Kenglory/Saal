972b97e4d96c1a4797c2513b6cab98f53e486196
kenglory <samusenele@gmail.com>
2025-06-17 03:41:55 +0200

    Aggiunta Layout nuovo parte 1

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+<link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet" />
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
